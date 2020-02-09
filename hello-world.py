34151caa59145d5ac1110bc5fd2fc9ff753a330a
Hunter Darling <46032371+hunter-darling@users.noreply.github.com>
2020-02-09 12:59:46 -0500

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  hello-world.py
+#  
+
+
+def main(agrs):
+	print("Hello World!")
+    return
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
